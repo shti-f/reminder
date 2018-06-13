de7a0b4649b6d3391ad0a7516ff7c6d60b5a01be
shti-f <shtif.dev@gmail.com>
2018-06-13 12:17:14 +0900

    initial commit

---
+++ reminder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  reminder
+//
+//  Created by NagaiNaoki on 2018/06/13.
+//  Copyright © 2018年 nmr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
